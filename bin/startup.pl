2c79dbbf873ef71776f52a961548905447f32bed
pef-secure <pef-secure@yandex.ru>
2014-12-11 21:22:31 +0100

    shebang

--- bin/startup.pl
+++ bin/startup.pl
@@ -1,6 +1,11 @@
+#!/usr/bin/perl
+
 use lib qw'app /home/pef/pef-front-psgi-dist/lib /home/pef/dbix-struct/lib';
+
 use Demo::AppFrontConfig;
 use PEF::Front::Preload;
 use DBIx::Struct (connector => 'PEF::Front::Connector');
+
 use PEF::Front::Route ('/index' => ['/appIndex', 'R']);
+
 PEF::Front::Route->to_app();
