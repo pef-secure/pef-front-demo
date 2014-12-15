2a4f98edf33763d7e94af7a07ab6bbed9be1fc4a
pef-secure <pef-secure@yandex.ru>
2014-12-15 18:58:10 +0100

    fix

--- bin/startup.pl
+++ bin/startup.pl
@@ -5,8 +5,8 @@ use lib qw'app /home/pef/pef-front-psgi-dist/lib /home/pef/dbix-struct/lib';
 use Demo::AppFrontConfig;
 use PEF::Front::Preload;
 use DBIx::Struct (
-	connector   => 'PEF::Front::Connector',
-	error_class => 'DBIx::Struct::Error::Hash'
+	connector_module => 'PEF::Front::Connector',
+	error_class      => 'DBIx::Struct::Error::Hash'
 );
 
 use PEF::Front::Route ('/index' => ['/appIndex', 'R']);
