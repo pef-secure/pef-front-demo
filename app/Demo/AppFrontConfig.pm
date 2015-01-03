a8f8ac3ecbf150ae9fbb16e91caff726315eb061
pef-secure <pef-secure@yandex.ru>
2015-01-03 17:00:30 +0100

    captacha font

--- app/Demo/AppFrontConfig.pm
+++ app/Demo/AppFrontConfig.pm
@@ -1,15 +1,16 @@
 package Demo::AppFrontConfig;
 
 our @EXPORT = qw{
-	demo_login_expires
+  demo_login_expires
 };
 
-sub demo_login_expires {'30 days'}
+# user params
+sub demo_login_expires { '30 days' }
 
-sub cfg_db_user                    {"pef"}
-sub cfg_db_password                {""}
-sub cfg_db_name                    {"pef"}
-
-
+# PEF::Front params
+sub cfg_db_user      { "pef" }
+sub cfg_db_password  { "" }
+sub cfg_db_name      { "pef" }
+sub cfg_captcha_font { "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono-Bold.ttf" }
 
 1;
