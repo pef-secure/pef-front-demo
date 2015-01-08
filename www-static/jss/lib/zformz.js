c99dadbc41f8feccb0cc003d4ec104a0ca391632
pef-secure <pef-secure@yandex.ru>
2015-01-08 19:40:25 +0100

    test js

---
+++ www-static/jss/lib/zformz.js
@@ -0,0 +1,34 @@
+// fields
+zFormz.fields = 'input:not([type="button"][type="submit"]),select,textarea';
+
+// get form data
+zFormz.getFormData = function(form) {
+	var formData = [];
+	form.find(zFormz.fields).each(
+		function() {
+			formData.push(encodeURIComponent($(this).attr('name')) + '=' + encodeURIComponent($(this).val()));
+	});
+	return formData.join('&');
+};
+
+zFormz.checkForm = function(form) {
+	return true;
+}
+
+zFormz.ajax = function(id_form, reactor) {
+	var form = $("#" + id_form);
+	form.submit ( function () {
+		if(zFormz.checkForm(form)) {
+			$.ajax({
+				url:      form.attr('action'),
+				data:     zFormz.getFormData(form),
+				dataType: 'json',
+				type:     form.attr('method'),
+				success:  function(response) {
+					ajax.response = response;
+					reactor();
+				}
+			});
+		}
+	});
+}
