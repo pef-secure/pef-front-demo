34770111d0385ccb3d3189ae1bd43bb65fece6b5
pef-secure <pef-secure@yandex.ru>
2015-01-14 14:04:23 +0100

    fixed possible html injection

--- app/Demo/InFilter/Default.pm
+++ app/Demo/InFilter/Default.pm
@@ -7,14 +7,18 @@ sub auth_to_author {
 	my $is_author = 0;
 	if ($def->{request}->cookies->{auth}) {
 		my $author = get_author_from_auth($def->{request}->cookies->{auth});
-		if($author) {
-			$field = $author->name;
+		if ($author) {
+			$field     = $author->name;
 			$is_author = 1;
 		}
 	}
-	if(!$is_author) {
+	if (!$is_author) {
+		$field =~ s/^\s*//;
+		$field =~ s/\s*$//;
+		$field =~ s/</&lt;/g;
+		$field =~ s/>/&gt;/g;
 		my $fake_author = one_row(author => {name => $field});
-		if($fake_author) {
+		if ($fake_author) {
 			$field = "(fake) $field";
 		}
 	}
