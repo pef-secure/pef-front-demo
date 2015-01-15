64396710b090d48e40e2e74ecd965c22f6e999cc
pef-secure <pef-secure@yandex.ru>
2015-01-15 10:16:32 +0100

    edit article routing

--- bin/startup.pl
+++ bin/startup.pl
@@ -15,7 +15,8 @@ use PEF::Front::Route (
 	'/'               => ['/index', 'R'],
 	qr'/index(.*)'    => '/appIndex$1',
 	qr'/article/(.*)' => '/appArticle/$1',
-	'/add'            => '/appAddArticle'
+	'/add'            => '/appAddArticle',
+	qr'/edit/(.*)'    => '/appEditArticle/$1'
 );
 
 PEF::Front::Route->to_app();
