63313843a08194123231d23462ce22de399f2717
pef-secure <pef-secure@yandex.ru>
2015-01-21 21:22:35 +0100

    removed debug

--- app/Demo/InFilter/Default.pm
+++ app/Demo/InFilter/Default.pm
@@ -3,25 +3,17 @@ use DBIx::Struct;
 use Demo::Common;
 use PEF::Front::Session;
 use Digest::MD5 qw(md5_hex);
-use Data::Dumper;
-use PEF::Front::Config;
 use strict;
 use warnings;
 
 sub auth_to_author {
 	my ($field, $def) = @_;
 	my $is_author = 0;
-print STDERR "*** auth_to_author *** | enter\n";
 	if ($def->{request}->cookies->{auth}) {
-print STDERR "*** auth_to_author *** | auth\n";
-print STDERR Dumper $def->{request}, $def->{request}->param(cfg_session_request_field());
 		my $session = PEF::Front::Session->new($def->{request});
-print STDERR Dumper $session, $session->data, $session->data->{name};
 		if (%{$session->data} && $session->data->{name}) {
-print STDERR "*** auth_to_author *** | session\n";
 			$field     = $session->data->{name};
 			$is_author = $session->data->{is_author};
-			print STDERR Dumper $session->data, $session->data->{is_oauth};
 			if ($session->data->{is_oauth}) {
 				$field = "anonymous-" . substr (md5_hex($field), 0, 4);
 			}
