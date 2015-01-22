943c4d26e6cd86a2decda43a96eb2a2378d878a7
pef-secure <pef-secure@yandex.ru>
2015-01-22 23:53:55 +0100

    added github

--- app/Demo/AppFrontConfig.pm
+++ app/Demo/AppFrontConfig.pm
@@ -11,19 +11,26 @@ sub demo_login_expires { '30 days' }
 
 # PEF::Front params
 sub cfg_session_ttl { state $ttl ||= parse_duration(demo_login_expires()); $ttl }
+
 sub cfg_oauth_client_id {
-	state $ids = {yandex => '87d188b469ab4fa1807556b757dd43ed'};
+	state $ids = {
+		yandex  => '87d188b469ab4fa1807556b757dd43ed',
+		git_hub => '9aaa615d2b89dab026a8'
+	};
 	$ids->{$_[0]};
 }
 
 sub cfg_oauth_client_secret {
-	state $secrets = {yandex => '8333c9c57f78446cabf8cbc603d82c43'};
+	state $secrets = {
+		yandex  => '8333c9c57f78446cabf8cbc603d82c43',
+		git_hub => 'ff1ba0d2ef9482ae38e4aee03ee38400a3e39cfc'
+	};
 	$secrets->{$_[0]};
 }
 
-sub cfg_db_user     { "pef" }
-sub cfg_db_password { "" }
-sub cfg_db_name     { "pef" }
+sub cfg_db_user      { "pef" }
+sub cfg_db_password  { "" }
+sub cfg_db_name      { "pef" }
 sub cfg_captcha_font { "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono-Bold.ttf" }
 
 1;
